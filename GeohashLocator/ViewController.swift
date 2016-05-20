a15cd004c2dbe74efb78d0a92bcb39aab9ea3774
Drew R. Hood <drew@drewrhood.me>
2016-05-20 05:05:06 -0700

    Initial Commit

---
+++ GeohashLocator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeohashLocator
+//
+//  Created by Drew Hood on 5/20/16.
+//  Copyright © 2016 Drew R. Hood. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
